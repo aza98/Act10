e3c24c19d2bb7e3869b75fa580f7b8739399434c
aza98 <Edgar.azael98@gmail.com>
2021-11-25 22:27:28 -0600

    Add files via upload

---
+++ Act10/Act10/Act10App.swift
@@ -0,0 +1,17 @@
+//
+//  Act10App.swift
+//  Act10
+//
+//  Created by Azael Reyna on 20/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Act10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Act10/Act10/TablaVC.swift
@@ -0,0 +1,8 @@
+//
+//  TablaVC.swift
+//  Act10
+//
+//  Created by Azael Reyna on 23/11/21.
+//
+
+import Foundation
